e526b76d5869c70815d8a8faaed9b5398a990240
Guest User <student@epicodus.com>
2015-11-12 17:15:38 -0800

    display user email that posted comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  belongs_to :user
 
   validates :comment_body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:soundcloud]
+
+  has_many :comments
 end
